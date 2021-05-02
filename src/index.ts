c76209d725b08bbafa3224c55fea8a5946b940da
Alejandro Nanez <alejonanez@gmail.com>
2021-05-02 10:29:53 -0500

    Add TODO items

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+/**
+ * TODO:
+ * - [ ] I want to create a schema from zero
+ * - [ ] I want to reset the schema
+ * - [ ] I want to update an existing schema
+ */
+
 console.log('hello there');
