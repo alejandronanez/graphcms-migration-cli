79253cfaeeae5d4365d9861f8bac1d656cc554bc
Alejandro Nanez <alejonanez@gmail.com>
2021-05-02 11:41:50 -0500

    Reading args from CLI

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 #!/usr/bin/env node
-
 /**
  * TODO:
  * - [ ] I want to create a schema from zero
  * - [ ] I want to reset the schema
  * - [ ] I want to update an existing schema
  */
+const yargs = require('yargs/yargs');
 
-console.log('hello there');
+const cliArguments = yargs(process.argv.slice(2)).options({
+  pat: {
+    type: 'string',
+    alias: 'permanentAuthToken',
+    demandOption: true,
+    description: `Permanent Auth Token used to run migrations against your GraphCMS space
+       please, refer to https://graphcms.com/docs/develop/management-sdk and follow the
+       instructions there to get it
+      `,
+  },
+}).argv;
+
+if (!cliArguments.pat) {
+  throw new Error('Permanent Auth Token is required');
+}
+
+console.log(`pat: ${cliArguments.pat}`);
